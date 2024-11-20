567846fafa61bfd3d761ed90573c893cf9571556
Eric Guerra <github.chef590@passmail.com>
2024-11-20 19:48:50 +0100

    change to ros2 humble

--- scripts/install/install_ros.sh
+++ scripts/install/install_ros.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Enable required repositories
+# Setup Sources
 sudo apt install software-properties-common
 sudo add-apt-repository universe
 
@@ -9,11 +9,10 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-# Install developement tools
-sudo apt update && sudo apt install ros-dev-tools
-
-# Install ROS2
+# Install ROS2 packages
 sudo apt update
 sudo apt upgrade
 
-sudo apt install ros-iron-desktop
+sudo apt install ros-humble-desktop
+
+sudo apt install ros-humble-desktop
